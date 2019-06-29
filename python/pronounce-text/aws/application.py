de6d8f191197b938ba4ff2581f2cae28471cd486
Michael Levin <mikhail.levin@gmail.com>
2019-06-29 03:58:18 +0300

    added pronounce-text skill

---
+++ python/pronounce-text/aws/application.py
@@ -0,0 +1,57 @@
+# coding: utf-8
+# Импортирует поддержку UTF-8.
+from __future__ import unicode_literals
+
+# Импортируем модули для работы с JSON и логами.
+import json
+import logging
+
+# Импортируем подмодули Flask для запуска веб-сервиса.
+from flask import Flask, request
+application = Flask(__name__)
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+# Хранилище данных о сессиях.
+sessionStorage = {}
+
+# Задаем параметры приложения Flask.
+@application.route("/", methods=['POST'])
+
+def main():
+# Функция получает тело запроса и возвращает ответ.
+    logging.info('Request: %r', request.json)
+
+    response = {
+        "version": request.json['version'],
+        "session": request.json['session'],
+        "response": {
+            "end_session": False
+        }
+    }
+
+    handle_dialog(request.json, response)
+
+    logging.info('Response: %r', response)
+
+    return json.dumps(
+        response,
+        ensure_ascii=False,
+        indent=2
+    )
+
+# Функция для непосредственной обработки диалога.
+def handle_dialog(req, res):
+    user_id = req['session']['user_id']
+
+    if req['session']['new']:
+        # Это новый пользователь.
+        # Поприветствуем его.
+        res['response']['text'] = 'Привет! Скажи мне фразу - и я прочитаю её своим голосом.'        
+        return
+
+    # Повторяем фразу пользователя в ответ
+    res['response']['text'] = '%s' % (
+        req['request']['original_utterance']
+    )
